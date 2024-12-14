d6576455ffd2d3fa24b0ee4cd5721b4f7f0f7c83
kathyli05 <kathyl200517@gmail.com>
2024-12-14 15:06:51 -0800

    delete unused listingpage file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -38,7 +38,6 @@ const App: React.FC = () => {
         {/* Routes */}
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="/product" element={<ListingPage />} /> */}
           <Route path="/user/" element={<UserProfile />} />
           {/* <Route path="/user/:id" element={<UserProfile />} /> */}
           <Route path="/product/:id" element={<ProductPage />} />
